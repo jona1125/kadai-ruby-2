3a8d6c9e89c57934650b92618246abcaaf98df29
Jona1125 <joe.horiguchi@gmail.com>
2021-12-23 06:00:33 +0000

    kadai-ruby20211223

--- kadai-fizzbuzz.rb
+++ kadai-fizzbuzz.rb
@@ -1,15 +1,17 @@
-num_max = 100
+#FizzBuzzのメソッドを作成しよう
+
 def fizzbuzz(n)
-if n % 15 == 0
-puts "FizzBuzz"
-elsif n % 3 == 0
-puts "Fizz"
-elsif n % 5 == 0
-puts "Buzz"
-else
-puts n
+      if n % 15 == 0
+          puts "FizzBuzz"
+      elsif n % 3 == 0
+          puts "Fizz"
+      elsif n % 5 == 0
+          puts "Buzz"
+      else
+          puts n
+      end
 end
-end
+num_max = 100
 (1..num_max).each do |n|
-fizzbuzz(n)
+  fizzbuzz(n)
 end
